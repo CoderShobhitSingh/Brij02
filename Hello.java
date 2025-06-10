62a716c0dbf289f9d5ef259c24167ee23303484d
CoderShobhitSingh <snghshobhit@gmail.com>
2025-06-10 12:21:56 +0530

    first

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("jai hind");
-        System.out.println("Ssyyssteeemmmmm");
-        System.out.println("bkl");
     }
 }
